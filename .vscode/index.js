7a6024f95d921c4e824094079f225b1c32f0f30e
shubham8528 <shubham.bisht@etelligens.in>
2021-08-18 21:07:13 +0530

    task (1), task(2) in file name index.js

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,1 +1,42 @@
 
+//task (1)..........
+// const getVal1=prompt("plzz enter First number=>>>>")
+// const getVal2=prompt("plzz enter Second number=>>>>")
+// const num=100;
+
+// let newval=Number(getVal1)+Number(getVal2)
+// alert(newval)
+// if(newval==num || getVal1==num || getVal2==num ){
+//    alert("true")
+// }else{alert("false")}
+
+
+
+//task (2)......
+// function value(num){
+
+//     for(let a=1; a<=num; a++){
+
+//         if(a%5===0 && a%3===0){
+//        document.write(`FiggBuss <br>`)
+//       continue;
+
+//         }
+//         else if(a%5==0){
+//           document.write(`Bugg <br>`)
+//       continue;
+
+//         }
+//         else if(a%3==0){
+//           document.write(`Figg <br>`)
+//       continue;
+
+//         }             
+//              document.write(a+"<br>")
+             
+//     }
+// }
+// const a= prompt("enter the value")
+// value(a)
+
+ 
